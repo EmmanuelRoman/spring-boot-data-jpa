1f04e65847878524cb79fc0bbb789db9f3497001
Emmanuel Román Santibáñez <emmanuel.roman.santibanez@gmail.com>
2018-09-27 00:02:24 -0500

    Creando la vista para listar a los clientes

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO clientes (id,nombre,apellido,email,create_at) VALUES (1,'Emmanuel','Román','eroman@gmail.com',TO_DATE('2018-09-26','yyyy-MM-DD'));
+INSERT INTO clientes (id,nombre,apellido,email,create_at) VALUES (2,'Emmanuel','Vera','evera@gmail.com',TO_DATE('2018-09-26','yyyy-MM-DD'));
+INSERT INTO clientes (id,nombre,apellido,email,create_at) VALUES (3,'Emmanuel','Torres','etorres@gmail.com',TO_DATE('2018-09-26','yyyy-MM-DD'));
+INSERT INTO clientes (id,nombre,apellido,email,create_at) VALUES (4,'Emmanuell','Vital','evital@gmail.com',TO_DATE('2018-09-26','yyyy-MM-DD'));
