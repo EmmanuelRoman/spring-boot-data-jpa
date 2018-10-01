f33c11a27d88d28fe4922ba78b233c6d91bd7198
Emmanuel Román Santibáñez <emmanuel.roman.santibanez@gmail.com>
2018-09-30 23:50:46 -0500

    Cambiando la configuracion para la base de datos a MySQL

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,4 @@
-INSERT INTO clientes (id,nombre,apellido,email,create_at) VALUES (1,'Emmanuel','Román','eroman@gmail.com',TO_DATE('2018-09-26','yyyy-MM-DD'));
-INSERT INTO clientes (id,nombre,apellido,email,create_at) VALUES (2,'Emmanuel','Vera','evera@gmail.com',TO_DATE('2018-09-26','yyyy-MM-DD'));
-INSERT INTO clientes (id,nombre,apellido,email,create_at) VALUES (3,'Emmanuel','Torres','etorres@gmail.com',TO_DATE('2018-09-26','yyyy-MM-DD'));
-INSERT INTO clientes (id,nombre,apellido,email,create_at) VALUES (4,'Emmanuell','Vital','evital@gmail.com',TO_DATE('2018-09-26','yyyy-MM-DD'));
+INSERT INTO clientes (id,nombre,apellido,email,create_at) VALUES (1,'Emmanuel','Román','eroman@gmail.com','2018-09-26');
+INSERT INTO clientes (id,nombre,apellido,email,create_at) VALUES (2,'Emmanuel','Vera','evera@gmail.com','2018-09-26');
+INSERT INTO clientes (id,nombre,apellido,email,create_at) VALUES (3,'Emmanuel','Torres','etorres@gmail.com','2018-09-26');
+INSERT INTO clientes (id,nombre,apellido,email,create_at) VALUES (4,'Emmanuell','Vital','evital@gmail.com','2018-09-26');
